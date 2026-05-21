a529c750cc2e745fb74a4ec731b44733edff08c8
irmelap2412 <irmela.prcic@fet.ba>
2026-05-21 18:48:59 +0200

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,17 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.node,  // <--- Dodano za Node.js (require, module...)
+        ...globals.jest   // <--- Dodano za Jest testove (test, expect...)
+      } 
+    } 
+  },
   pluginReact.configs.flat.recommended,
 ]);
